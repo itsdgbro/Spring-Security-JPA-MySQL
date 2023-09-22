eee730a6d72287478b29650579ffecf6787dd84c
Dipak Gurung <dipakgrg524@gmail.com>
2023-09-22 19:31:22 +0545

    Initial commit

---
+++ src/main/java/com/mysqlsecurity/SpringSecurityJpaMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.mysqlsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpaMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaMySqlApplication.class, args);
+	}
+
+}
