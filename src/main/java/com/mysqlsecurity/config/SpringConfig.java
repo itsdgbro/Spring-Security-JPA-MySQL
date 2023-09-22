95189b372a08df5f7da0aecceca75253bcfa4274
Dipak Gurung <dipakgrg524@gmail.com>
2023-09-22 22:55:01 +0545

    BLA

--- src/main/java/com/mysqlsecurity/config/SpringConfig.java
+++ src/main/java/com/mysqlsecurity/config/SpringConfig.java
@@ -2,7 +2,9 @@ package com.mysqlsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,12 +14,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringConfig {
 
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
     // BCryptPasswordEncoder is needed when data is store from form cus Spring security will expect BCrypt-ed password in the database/storage, for now this method is not used
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -25,9 +33,12 @@ public class SpringConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN"))
-                .formLogin(Customizer.withDefaults());
+                .formLogin(login -> login
+                        .loginPage("/auth/login")
+                        .defaultSuccessUrl("/user"));
         return http.build();
     }
 
--- src/main/java/com/mysqlsecurity/controller/UserController.java
+++ src/main/java/com/mysqlsecurity/controller/UserController.java
@@ -1,10 +1,29 @@
 package com.mysqlsecurity.controller;
 
+import com.mysqlsecurity.model.User;
+import com.mysqlsecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/")
     public String home(){
@@ -12,12 +31,30 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    public String getUser(){
-        return "<h1>User Page</h1>";
+    public String getUser(Principal principal){
+        return "<h1>User Page " + principal.getName() + "</h1>";
     }
 
     @GetMapping("/admin")
     public String getAdmin(){
         return "<h1>Admin Page</h1>";
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) throws Exception{
+        String username = credentials.get("Username");
+        String password = credentials.get("Password");
+
+        //  Load user details using UserDetailsService
+        UserDetails userDetails = userService.loadUserByUsername(username);
+
+        // generate token
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, password);
+
+        // authenticate
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        // Set authentication in SecurityContext
+        return ResponseEntity.ok("Success");
     }
 }
