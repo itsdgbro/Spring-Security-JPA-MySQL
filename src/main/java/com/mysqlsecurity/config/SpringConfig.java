f82ff2a1a01de9d9eb82aa0d4e06c763fabd160b
Dipak Gurung <dipakgrg524@gmail.com>
2023-09-23 18:20:29 +0545

    CCC

--- src/main/java/com/mysqlsecurity/config/SpringConfig.java
+++ src/main/java/com/mysqlsecurity/config/SpringConfig.java
@@ -1,13 +1,18 @@
 package com.mysqlsecurity.config;
 
+import com.mysqlsecurity.model.CustomUserDetails;
+import com.mysqlsecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,9 +21,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringConfig {
 
+    @Autowired
+    private UserService userService;
+
     @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration configuration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
@@ -35,13 +42,9 @@ public class SpringConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/").permitAll()
-//                        .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN"))
-                .formLogin(login -> login
-//                        .loginPage("/auth/login")
-                        .defaultSuccessUrl("/user")
-                        .permitAll());
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
